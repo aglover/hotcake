531c036a6de03549f68e9928f603088057a775c9
Andrew Glover <ajglover@gmail.com>
2023-05-15 14:41:12 -0700

    working on sinatra tests

--- lib/tag_app.rb
+++ lib/tag_app.rb
@@ -1,8 +1,13 @@
 require "sinatra"
 require "sinatra/json"
+require File.expand_path(File.dirname(__FILE__) + '/taggable')
 
 
+get "/applications/:environment/:app_name" do |env, app|
+  factory = Taggable::TaggableFactory.new()
+  tag_thing = factory.taggableInstance()
+  
+  taggable_result = Taggable::SearchableApplication.find_by_name_and_env app, env
 
-get "/" do
-  json message: "Hello world"
+  json taggable_result.as_document
 end
--- spec/int/sinatra_spec.rb
+++ spec/int/sinatra_spec.rb
@@ -2,20 +2,46 @@ ENV['APP_ENV'] = 'test'
 
 require 'rspec'
 require 'rack/test'
+require 'json'
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/tag_app')
+require File.expand_path(File.dirname(__FILE__) + '/../../lib/taggable')
 
-RSpec.describe "Simple Hello World" do
+RSpec.describe "Tag Application" do
     include Rack::Test::Methods
   
     def app
       Sinatra::Application
     end
   
-    it "says hello" do
-      get "/"
+
+    before(:each) do
+      factory = Taggable::TaggableFactory.new()
+      tag_thing = factory.taggableInstance()
+      tag_thing.name = "bluespar"
+      tag_thing.env = "prod"
+      tag_thing.infra_type = "cluster"
+      tag_thing.infra_name = "MAIN"
+      tag_thing.tags =  ["vul1", "deprecated"]
+      tag_thing.props = {:stack => "TEST"}
+      doc_id = tag_thing.save
+      sleep 1
+    end
+
+    after(:each) do
+      factory = Taggable::TaggableFactory.new()
+      tag_thing = factory.taggableInstance()
+      tag_thing.name = "bluespar"
+      tag_thing.env = "prod"
+      tag_thing.delete
+    end
+
+    it "responds with an application document from a specific environment" do
+      get "/applications/prod/bluespar"
       expect(last_response).to be_ok
-      expect(last_response.body).to eq({message:"Hello world"}.to_json)
       expect(last_response.headers['Content-Type']).to eq "application/json"
+      hash = JSON.parse(JSON.parse (last_response.body))
+      expect(hash['application']).to eq "bluespar"
+      expect(hash['infra_type']).to eq "cluster"
     end
     
   end
